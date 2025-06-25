ea8aa346f38afe333edb059c78d1fe02e7a356f2
JVCM123 <jvcmoura2012@gmail.com>
2025-06-25 19:27:07 -0300

    estruturação back-end

---
+++ Config/DependencyInjectionConfig.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.DynamoDBv2;
+using FraudSys.Core.Notifications;
+using System.Runtime.CompilerServices;
+
+namespace FraudSys.MVC.Config
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<Notifier>();
+            services.AddSingleton<IAmazonDynamoDB>();
+
+            return services;
+        }
+    }
+}
